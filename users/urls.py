d75becd5218f7eb9541cd1daf149c9ea94b2ec6d
Gokudoma <stephan_wacker@hotmail.com>
2025-11-11 11:13:40 +0100

    feat(users): Add login and email-check endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, CustomAuthToken, EmailCheckView
 
 urlpatterns = [
     path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
+    path('email-check/', EmailCheckView.as_view(), name='email-check'),
 ]
